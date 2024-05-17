c76df1e8bb0ffda06b1bbda4d7b05d7192246b75
fleetmouse <omar.jamal45@gmail.com>
2024-05-17 08:28:01 +0300

    Update home view with new book cover images and widget modifications

--- lib/features/home/data/presentation/view/wedigts/home_view_body.dart
+++ lib/features/home/data/presentation/view/wedigts/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:bookapp/core/utils/styles.dart';
-import 'package:bookapp/features/home/data/presentation/view/wedigts/bestseller.dart';
+import 'package:bookapp/features/home/data/presentation/view/wedigts/bestseller_listView.dart';
 import 'package:bookapp/features/home/data/presentation/view/wedigts/custom_app_bar.dart';
 import 'package:bookapp/features/home/data/presentation/view/wedigts/listview_up_part.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
@@ -11,22 +12,38 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30), // up or side - symertric
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppbar(), // custom app bar in wedgit file
-          BooksListView(), //list view using the homeuplist wedigt
-          Gap(50),
-          Text(
-            'Best Seller',
-            style: Style.titlesec,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 30), // make padding const it repet alot
+                child: CustomAppbar(),
+              ), // custom app bar in wedgit file
+              BooksListView(), //list view using the homeuplist wedigt
+              Gap(50),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: Style.titlesec,
+                ),
+              ),
+              Gap(20),
+            ],
           ),
-          Gap(20),
-          BestSellerListView()
-        ],
-      ),
+        ),
+        SliverFillRemaining(
+          //casue its at the bottom we use fill
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
+          ),
+        )
+      ],
     );
   }
 }
