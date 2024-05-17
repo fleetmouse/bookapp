325a8bf2702772d24900b6e26e4ab44d637cae3d
fleetmouse <omar.jamal45@gmail.com>
2024-05-17 03:17:06 +0300

    Update home view with new book cover images and widget modifications

--- lib/features/home/data/presentation/view/wedigts/home_view_body.dart
+++ lib/features/home/data/presentation/view/wedigts/home_view_body.dart
@@ -1,11 +1,10 @@
-import 'package:bookapp/core/utils/assets.dart';
 import 'package:bookapp/core/utils/styles.dart';
+import 'package:bookapp/features/home/data/presentation/view/wedigts/bestseller.dart';
 import 'package:bookapp/features/home/data/presentation/view/wedigts/custom_app_bar.dart';
 import 'package:bookapp/features/home/data/presentation/view/wedigts/listview_up_part.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -26,49 +25,6 @@ class HomeViewBody extends StatelessWidget {
           ),
           Gap(20),
           BestSellerListView()
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.test3),
-                  fit: BoxFit.fill,
-                ), // so the pic cant take the container size
-              ),
-            ),
-          ),
-          const Gap(30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'The Godfather Mario Puzo',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.merriweather(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
