2674e11ecf27d06040606829a40c6f65c242507c
fleetmouse <omar.jamal45@gmail.com>
2024-05-19 05:25:01 +0300

    feat: Implement fetchBestSellerBooks method in HomeRepoImpl
    - Added API service dependency to HomeRepoImpl constructor
    - Implemented fetchBestSellerBooks method to make a GET request to the Google Books API and return a list of BooksModel
    - Handled exceptions and returned appropriate failure types

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookapp/core/errors/Fail.dart';
+import 'package:bookapp/core/utils/api_service.dart';
 import 'package:bookapp/features/home/data/models/books_model/books_model.dart';
 import 'package:bookapp/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final APIService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() async {
+    try {
+      var data =
+          await apiService.get(endPoint: 'volumes?q=flutter&orderBy=newest');
+
+      List<BooksModel> Books = [];
+
+      for (var item in data['items']) {
+        Books.add(BooksModel.fromJson(item));
+      }
+
+      return right(Books); // this from the pacakge that we use dartz
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
@@ -16,6 +32,5 @@ class HomeRepoImpl implements HomeRepo {
     throw UnimplementedError();
   }
 }
-
 
 // we need to make a warper so we make connection with the service
